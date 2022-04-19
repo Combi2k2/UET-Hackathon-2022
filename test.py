02b620e39223667b96ea2c2ec0ec47b003eb7533
Combi2k2 <cumbee2k2@gmail.com>
2022-04-19 14:05:09 +0200

    Add a dictionary to get coordinate of address

--- test.py
+++ test.py
@@ -1,54 +1,16 @@
-from geopy.geocoders import Nominatim
+import pandas as pd
+import json
+import numpy as np
 
-geolocator = Nominatim(user_agent="geoapiExercises")
+address = pd.read_csv('address_work.csv')
+address_dict = dict()
 
-location = geolocator.geocode('hà nam')
+for i in range(len(address)):
+    name = address['address'][i]
+    lat = address['latitude'][i]
+    lon = address['longitude'][i]
 
-print(location)
+    address_dict[name] = {"latitude": lat, "longitude": lon}
 
-x = location.raw['lat']
-y = location.raw['lon']
-
-print(x, y)
-
-
-address_Hanoi = ['hn',
-                 'tt138a giảng vừ',
-                 'hai bà t',
-                 'ngõ 409 kim mã',
-                 'láng hạ',
-                 'thanh oai',
-                 'hà tây',
-                 'hà đông',
-                 'ngõ 379 phố hoàng hoa thám',
-                 'khu đô thị mới dịch vọng',
-                 'tòa nhà lotte center',
-                 'a mandarin garden hoàng minh giám',
-                 'hà nộ',
-                 'khu chung cư việt hưng',
-                 'ngõ 668 đường lạc long quân',
-                 'tp ha nô i',
-                 '1 khu đt việt hưng',
-                 'hà n i',
-                 'th nh phè h néi',
-                 'đống đa',
-                 'h1 tt qđ phương mai',
-                 'hà nam',
-                 'hn.',
-                 'ngách 445 42 đường nguyễn khang',
-                 'lô cc6 khu dịch vụ tổng hợp và nhà ở hồ linh đàm',
-                 'ngõ 140 1 6 nguyễn xiển',
-                 'ngách 71 46 hoàng văn thái',
-                 'quận thanh',
-                 'hà nội 52743',
-                 'chung cư chelsea park',
-                 'khu tỏi định cư đường 32',
-                 'ngõ 11 tổ dân phố số 4',
-                 'tổ dân phố số 6',
-                 'thôn ngãi cầu',
-                 'thôn dương cốc']
-
-address_HoaBinhf = ['t hòa bình',
-                    't. hòa bình',
-                    't.hoà bình',
-                    'hb']
+with open('address_dict.json', 'w', encoding = 'utf-8') as f:
+    json.dump(address_dict, f, ensure_ascii = False, indent = 4)
